15499cdb6d713f1716b26165f28040b5be339352
couturecraigj <couturecraigj@gmail.com>
2018-10-08 15:01:23 -0400

    Again trying to make the logo look better

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,18 @@ import styled from 'styled-components'
 
 const Strong = styled.div`
   transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
-  background-color: #ffffff;
+  background-color: #ddd;
   display: inline-block;
   line-height: 0.5em;
-  margin: 1em 0 0 0;
-  padding: 0.35em 0.8em;
+  margin: 0.75vw 0 0 0.5vw;
+  padding: 0 1.4vw;
   border-radius: 0.125em;
   &:hover {
     background-color: #9bf1ff !important;
+  }
+  img {
+    /* height: 20%; */
+    width: calc(1.5vw + 25px);
   }
 `
 
@@ -22,7 +26,7 @@ const query = graphql`
     file(absolutePath: { regex: "/Logo/" }) {
       id
       childImageSharp {
-        resize(width: 23) {
+        resize(width: 61) {
           src
         }
       }
